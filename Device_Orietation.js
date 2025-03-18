2fad0fdb788457eb8fca772e8e0cf71ad2b965cd
kenzieshane <111677159+kenzieshane@users.noreply.github.com>
2025-03-18 20:21:39 +0700

    litteraly LOL

---
+++ Device_Orietation.js
@@ -0,0 +1,44 @@
+(function (Scratch) {
+  'use strict';
+
+  const vm = Scratch.vm;
+  const runtime = vm.runtime;
+  const isPackaged = runtime.isPackaged;
+
+  let allowJSCode = true;
+  let ineditor = true;
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('This extension must run unsandboxed');
+  }
+  
+  class CustomJS {
+    getInfo() {
+      return {
+        id: 'PhoneDeviceOr',
+        name: 'Device Orientation',
+        color1: '#AAAAAA',
+        blocks: [
+          // this is the main code.
+          {
+            opcode: 'getOrientation',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'device orientation',
+            func: 'getOrientation'
+          }
+        ]
+      };
+    }
+
+    // function to detect and return orientation
+    getOrientation() {
+      if (window.innerWidth > window.innerHeight) {
+        return 'horizontal';
+      } else {
+        return 'vertical';
+      }
+    }
+  }
+
+  Scratch.extensions.register(new CustomJS());
+})(Scratch);
